38b4a1c46e2704c707d747ed981deb795ffd51c6
lauraosallor <lauraosallor@gmail.com>
2024-06-26 10:31:14 +0200

    corrección

--- src/js/index.js
+++ src/js/index.js
@@ -14,80 +14,73 @@ rootStyles.setProperty('--primary-color', 'green');
 // - Crea un div de color rojo y colócalo en la parte superior izquierda de tu web. Haz que tu web tenga un min-height de 500vh para generar scroll. El div deberá sincronizarse con el scroll, si estás arriba del todo medirá 0 de ancho y si está abajo del todo medirá el 100%, según vayas haciendo scroll el div deberá ir creciendo o encogiendo en función de si subes o bajas.
 
 const rootStyles = document.documentElement.style;
-let divGrowth = ''
+let divGrowth = '';
+let scrollAmount = '';
 
 const getPercentageScroll = () => {
   let maxScrollPosition = document.body.scrollHeight - window.innerHeight;
-
-  let scrollAmount = window.scrollY;
+  scrollAmount = window.scrollY;
   let scrollPercentage = (scrollAmount * 100) / maxScrollPosition;
 
   const maxWidthWindow = window.innerWidth;
   divGrowth = (scrollPercentage * maxWidthWindow) / 100;
 
-  rootStyles.setProperty('--div-width', String(divGrowth) + 'px');
-
-  return divGrowth
+  rootStyles.setProperty('--div-width', divGrowth + 'px');
 };
 
 window.addEventListener('scroll', getPercentageScroll);
-const a = 80;
 
 // - Añade un h1 al ejercicio anterior que te diga cuantos px te has desplazado.
 
-const displacementElement = document.getElementById('displacement')
+const displacementElement = document.getElementById('displacement');
 
-const pxChange = () =>{
-    displacementElement.textContent =Math.round(divGrowth) + 'px'
-}
+const pxChange = () => {
+  displacementElement.textContent = Math.round(scrollAmount) + 'px';
+};
 
-window.addEventListener('scroll', pxChange)
-
+window.addEventListener('scroll', pxChange);
 
 // - Crea dos botones en tu web para que al pulsarlos generen un color aleatorio para el body y se aplique. Haz una función para generarlo en RGB y otra para generarlo en hexadecimal y ejecuta cada una desde un botón
 
-const buttonRGBElement = document.getElementById('button-rgb')
-const buttoHEXElement = document.getElementById('button-hex')
+const buttonRGBElement = document.getElementById('button-rgb');
+const buttoHEXElement = document.getElementById('button-hex');
 
-const colorsRGB = () =>{
-    let colorRGB = '';
-    for (let i = 0; i<3;i++){
-        let randomNumber = Math.floor(Math.random()*255)
-        colorRGB += randomNumber +','
+const colorsRGB = () => {
+  let colorRGB = '';
+  for (let i = 0; i < 3; i++) {
+    let randomNumber = Math.floor(Math.random() * 255);
+    colorRGB += randomNumber + ',';
 
-        if( i === 2){
-            colorRGB += randomNumber
-        }
-        
+    if (i === 2) {
+      colorRGB += randomNumber;
     }
-    rootStyles.setProperty('--background-color', `rgb(${colorRGB})`)
-}
+  }
+  rootStyles.setProperty('--background-color', `rgb(${colorRGB})`);
+};
 
-buttonRGBElement.addEventListener('click',colorsRGB)
+buttonRGBElement.addEventListener('click', colorsRGB);
 
-const colorsHEX = () =>{
-    let posibleCharacters = '0123456789abcdef'
-    let colorHEX = '#'
+//hacer el bucke es sobre complicarse, se pueden escribir los de math random directamente ---> `rgb (${red}, ${green}, ${blue}) ---> red = Math.floor(Matn.random()*255)
 
-    for (let i = 0; i<3;i++){
-        let index = Math.floor(Math.random()*posibleCharacters.length - 1)
-        colorHEX += posibleCharacters.charAt(index)
-    }
+const colorsHEX = () => {
+  const posibleCharacters = '0123456789abcdef';
+  let colorHEX = '#';
 
-    rootStyles.setProperty('--background-color', colorHEX)
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * posibleCharacters.length - 1);
+    colorHEX += posibleCharacters.charAt(index);
+  }
 
-}
+  rootStyles.setProperty('--background-color', colorHEX);
+};
 
-buttoHEXElement.addEventListener('click', colorsHEX)
+buttoHEXElement.addEventListener('click', colorsHEX);
 
 // - Crea un div de 20px x 20px y sincronizalo con el movimiento del ratón, el div deberá quedarse pegado a la flecha de tu ratón y moverse junto a él.
 
-const cursorElement = document.getElementById('cursor')
+const trackMovement = event => {
+  rootStyles.setProperty('--left-position', event.x + 'px');
+  rootStyles.setProperty('--top-position', event.y + 'px');
+};
 
-const trackMovement = (event) =>{
-    rootStyles.setProperty('--left-position', event.clientX + 2 + 'px')
-
-    rootStyles.setProperty('--top-position', event.clientY + 2 +'px')
-}
-
-window.addEventListener('mousemove', trackMovement)
+window.addEventListener('mousemove', trackMovement);
